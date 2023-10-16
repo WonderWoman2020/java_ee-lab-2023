063584252f3ee75a09300c544fe320f64744344a
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-10-16 21:15:08 +0200

    Some experimenting with many servlets approach

--- Lab1/src/main/java/controller/UsersServlet.java
+++ Lab1/src/main/java/controller/UsersServlet.java
@@ -8,22 +8,18 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-@WebServlet("/api/*")
-public class MainServlet extends HttpServlet {
+@WebServlet("/api/users/*")
+public class UsersServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=UTF-8");
-
-        /*PrintWriter out = resp.getWriter();
-        out.write("<h1>");
-        out.write("Hello world!");
-        out.write("<h1>");
-        out.write("<h2>"+req.getPathInfo()+"<h2>");*/
-        //getServletContext().getRequestDispatcher("/api/users/*").forward(req, resp);
-        resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        PrintWriter out = resp.getWriter();
+        out.write("<h2>");
+        out.write("Hello users!");
+        out.write("<h2>");
+        out.write("<h2>"+req.getPathInfo()+"<h2>");
+        out.write("<h2>"+req.getServletPath()+"<h2>");
     }
 }

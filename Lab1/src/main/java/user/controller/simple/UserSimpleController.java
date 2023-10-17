8aeb4ff3718daf1d7ab623907ef429d44fe57460
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-10-17 04:29:02 +0200

    Removed some commented code

--- Lab1/src/main/java/user/controller/simple/UserSimpleController.java
+++ Lab1/src/main/java/user/controller/simple/UserSimpleController.java
@@ -10,10 +10,6 @@ import user.dto.function.UserToResponseFunction;
 import user.dto.function.UsersToResponseFunction;
 import user.entity.User;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -31,19 +27,6 @@ public class UserSimpleController implements UserController {
     private DataStore store; // tymczas.
     @Override
     public GetUsersResponse getUsers() {
-        /*List<User> users = new ArrayList<>();
-        users.add(User.builder()
-                .id(UUID.fromString("12345678-AAAA-AAAA-AAAA-123456789ABC"))
-                .nick("First!")
-                .login("haha-login")
-                .password("Don'tHackMe!123")
-                .birthDate(LocalDate.now())
-                .roles(null)
-                .reputation(10)
-                .tutorial(null)
-                .build());
-
-        return new UsersToResponseFunction().apply(users);*/
         return new UsersToResponseFunction().apply(findAll());
     }
 

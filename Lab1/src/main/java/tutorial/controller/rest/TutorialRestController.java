b2cf6d4f57bb550e1e2f0f820de26c24313196e7
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-11-14 22:11:35 +0100

    Added redirection in PUT element (tutorial) request, to implement POST-REDIRECT-GET rule

--- Lab1/src/main/java/tutorial/controller/rest/TutorialRestController.java
+++ Lab1/src/main/java/tutorial/controller/rest/TutorialRestController.java
@@ -1,12 +1,18 @@
 package tutorial.controller.rest;
 
 //import controller.exception.NotFoundException;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.enterprise.inject.Alternative;
 import jakarta.enterprise.inject.Default;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
 import tutorial.controller.api.TutorialController;
 import tutorial.dto.GetTutorialResponse;
 import tutorial.dto.GetTutorialsResponse;
@@ -28,9 +34,28 @@ public class TutorialRestController implements TutorialController {
 
     private TutorialService service;
 
+    /**
+     * Allows to create {@link UriBuilder} based on current request.
+     */
+    private final UriInfo uriInfo;
+
+    /**
+     * Current HTTP Servlet response.
+     */
+    private HttpServletResponse response;
+
+    @Context
+    public void setResponse(HttpServletResponse response) {
+        //ATM in this implementation only HttpServletRequest can be injected with CDI so JAX-RS injection is used.
+        this.response = response;
+    }
+
     @Inject
-    public TutorialRestController(TutorialService service) {
+    public TutorialRestController(TutorialService service,
+                                  @SuppressWarnings("CdiInjectionPointsInspection") UriInfo uriInfo)
+    {
         this.service = service;
+        this.uriInfo = uriInfo;
     }
 
     @Override
@@ -83,6 +108,16 @@ public class TutorialRestController implements TutorialController {
             uuids.put("tutorialId", tutorialId);
             uuids.put("skillId", skillId);
             service.create(new RequestToTutorialFunction().apply(uuids, request));
+
+            response.setHeader("Location", uriInfo.getBaseUriBuilder()
+                    .path(TutorialController.class, "getTutorialBySkill")
+                    .build(skillId, tutorialId)
+                    .toString());
+            //This can be done with Response builder but requires method different return type.
+            //Calling HttpServletResponse#setStatus(int) is ignored.
+            //Calling HttpServletResponse#sendError(int) causes response headers and body looking like error.
+            throw new WebApplicationException(Response.Status.CREATED);
+
         } catch (IllegalArgumentException ex) {
             throw new BadRequestException(ex);
         }
